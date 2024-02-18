1cf69c052876719b52c6a1dc3b8730bb5e74643b
bugha123123 <irakliberdzena314@gmail.com>
2024-02-18 21:24:03 +0400

    .

---
+++ CinemaClix/Controllers/ShowController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CinemaClix.Controllers
+{
+    public class ShowController : Controller
+    {
+        public IActionResult Shows()
+        {
+            return View();
+        }
+    }
+}
